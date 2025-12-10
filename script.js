eea522c9a3636ec08c75c12e761ae984045bc551
Zhanna <kezhanna.03@mail.ru>
2025-12-10 11:02:02 +0500

    task 46

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ const getRange = (array) => {
   return Math.max(...array) - Math.min(...array);
 }
 
+
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
@@ -37,10 +39,10 @@ const calculate = () => {
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
-
+  const range = getRange(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
-
+  document.querySelector("#range").textContent = range;
 }
